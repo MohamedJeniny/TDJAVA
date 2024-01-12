6a1ba57d6ece35f8f9b7193d4a95bb0c50c6dad5
Mohamed jeniny <mohamedjeniny01@gmail.com>
2024-01-12 17:23:44 +0100

    COMMIT

---
+++ src/TD2/EX6.java
@@ -0,0 +1,43 @@
+package TD2;
+
+public class EX6 {  public static void main(String[] args) {
+    Banque b=new Banque(1,5000,"AB12345");
+    b.toString();
+    b.solde;
+    b.diposer(500);
+    b.toString();
+    b.solde;
+
+}
+
+    class Banque{
+        int NCompte;
+        double solde;
+        String cin;
+
+        public Banque(int NCompte, double solde, String cin) {
+            this.NCompte = NCompte;
+            this.solde = solde;
+            this.cin = cin;
+        }
+
+        public void diposer(double somme){
+            this.solde += somme;
+        }
+        public boolean retirer(double somme){
+            if((solde - somme)>= 0) {
+                solde -= somme;
+                return  true;
+            }
+            return false;
+        }
+
+        @Override
+        public String toString() {
+            return "Banque{" +
+                    "NCompte=" + NCompte +
+                    ", solde=" + solde +
+                    ", cin='" + cin + '\'' +
+                    '}';
+        }
+    }
